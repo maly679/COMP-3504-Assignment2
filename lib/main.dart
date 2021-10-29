e767e5d6bd5e033f2a6769a0bab24ac86af56b43
msuja061 <msuja061@mtroyal.ca>
2021-10-29 12:42:24 -0600

    Created function to go to an AddItemPage, created form for item and initialized it. TODO: add items to array

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-                title: Text('Inventory Tracking System')
-            ),
-            body: Center(
-                child: ListSearch()
-            )
-        )
-    );
+            appBar: AppBar(title: Text('Inventory Tracking System')),
+            body: Center(child: ListSearch())));
   }
 }
 
 class ListSearch extends StatefulWidget {
+  @override
   ListSearchState createState() => ListSearchState();
 }
 
@@ -31,9 +26,11 @@ class ListSearchState extends State<ListSearch> {
   List<Item> Items = [];
   static List<String> mainDataList = [];
 
+  final _itemID = TextEditingController();
+
   @override
   void initState() {
-    _loadData().then((value){
+    _loadData().then((value) {
       print('Async done');
     });
     super.initState();
@@ -47,44 +44,41 @@ class ListSearchState extends State<ListSearch> {
     result = _data.split("\r\n");
     for (var i = 0; i < result.length; i++) {
       var splitResult = result[i].split(';');
-      var item = new Item (
-          int.parse(splitResult[0]), splitResult[1], int.parse(splitResult[2]),
-          double.parse(splitResult[3]), int.parse(splitResult[4]));
+      var item = new Item(
+          int.parse(splitResult[0]),
+          splitResult[1],
+          int.parse(splitResult[2]),
+          double.parse(splitResult[3]),
+          int.parse(splitResult[4]));
       Items.add(item);
       mainDataList.add(item.name.toString());
-
     }
-
   }
 
+  // Future<void> _addData() async {
+  //   Items.add(itemCreated);
+  // }
 
-    TextEditingController _textController = TextEditingController();
+  TextEditingController _textController = TextEditingController();
 
-    // Copy Main List into New List.
-    List<String> newDataList = List.from(mainDataList);
+  // Copy Main List into New List.
+  List<String> newDataList = List.from(mainDataList);
 
-    onItemChanged(String value) {
-      setState(() {
-        newDataList = mainDataList
-            .where((string) =>
-            string.toLowerCase().contains(value.toLowerCase()))
-            .toList();
-      });
-    }
+  onItemChanged(String value) {
+    setState(() {
+      newDataList = mainDataList
+          .where((string) => string.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
 
-
-    showDetails(String Name) {
-
-      for (int i = 0; i < Items.length; i++) {
-
-        if (Items[i].name == Name) {
-          print(Items[i]);
-        }
-
+  showDetails(String Name) {
+    for (int i = 0; i < Items.length; i++) {
+      if (Items[i].name == Name) {
+        print(Items[i]);
       }
     }
-
-
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -107,8 +101,24 @@ class ListSearchState extends State<ListSearch> {
               children: newDataList.map((data) {
                 return ListTile(
                   title: Text(data),
-                  onTap: ()=> showDetails(data),);
+                  onTap: () => showDetails(data),
+                );
               }).toList(),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(25),
+            child: FlatButton(
+              child: Text(
+                'Add Item to Inventory',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const AddItemPage()),
+                );
+              },
             ),
           )
         ],
@@ -129,33 +139,135 @@ class _SearchAppBarState extends State<SearchAppBar> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-          centerTitle: true,
-          title:appBarTitle,
-          actions: <Widget>[
-            new IconButton(icon: actionIcon,onPressed:(){
-              setState(() {
-                if ( this.actionIcon.icon == Icons.search){
-                  this.actionIcon = new Icon(Icons.close);
-                  this.appBarTitle = new TextField(
-                    style: new TextStyle(
-                      color: Colors.white,
+      appBar:
+          new AppBar(centerTitle: true, title: appBarTitle, actions: <Widget>[
+        new IconButton(
+          icon: actionIcon,
+          onPressed: () {
+            setState(() {
+              if (this.actionIcon.icon == Icons.search) {
+                this.actionIcon = new Icon(Icons.close);
+                this.appBarTitle = new TextField(
+                  style: new TextStyle(
+                    color: Colors.white,
+                  ),
+                  decoration: new InputDecoration(
+                      prefixIcon: new Icon(Icons.search, color: Colors.white),
+                      hintText: "Search...",
+                      hintStyle: new TextStyle(color: Colors.white)),
+                );
+              } else {
+                this.actionIcon = new Icon(Icons.search);
+                this.appBarTitle = new Text("AppBar Title");
+              }
+            });
+          },
+        ),
+      ]),
+    );
+  }
+}
 
-                    ),
-                    decoration: new InputDecoration(
-                        prefixIcon: new Icon(Icons.search,color: Colors.white),
-                        hintText: "Search...",
-                        hintStyle: new TextStyle(color: Colors.white)
-                    ),
-                  );}
-                else {
-                  this.actionIcon = new Icon(Icons.search);
-                  this.appBarTitle = new Text("AppBar Title");
-                }
+class AddItemPage extends StatelessWidget {
+  const AddItemPage({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    final _itemID = TextEditingController();
+    final _itemName = TextEditingController();
+    final _itemQuantity = TextEditingController();
+    final _itemPrice = TextEditingController();
+    final _supplierID = TextEditingController();
 
-              });
-            } ,),]
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add Item to Inventory"),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+                width: 280,
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: _itemID,
+                  autocorrect: true,
+                  decoration: InputDecoration(hintText: 'Enter Item ID'),
+                )),
+            Container(
+                width: 280,
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: _itemName,
+                  autocorrect: true,
+                  decoration: InputDecoration(hintText: 'Enter Item Name'),
+                )),
+            Container(
+                width: 280,
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: _itemQuantity,
+                  autocorrect: true,
+                  decoration: InputDecoration(hintText: 'Enter Item Quantity'),
+                )),
+            Container(
+                width: 280,
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: _itemPrice,
+                  autocorrect: true,
+                  decoration: InputDecoration(hintText: 'Enter Item Price'),
+                )),
+            Container(
+                width: 280,
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: _supplierID,
+                  autocorrect: true,
+                  decoration: InputDecoration(hintText: 'Enter Supplier ID'),
+                )),
+            RaisedButton(
+                onPressed: () {
+                  print(_itemID.text +
+                      " " +
+                      _itemName.text +
+                      " " +
+                      _itemQuantity.text +
+                      " " +
+                      _itemPrice.text +
+                      " " +
+                      _supplierID.text);
+
+                  // todo: add item to array
+                  var item = new Item(
+                      int.parse(_itemID.text),
+                      _itemName.text,
+                      int.parse(_itemQuantity.text),
+                      double.parse(_itemPrice.text),
+                      int.parse(_supplierID.text));
+
+                  // Items.add(item);
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ListSearch(),
+                      ));
+
+                  // Return to main menu
+                  // Navigator.pop(context);
+                },
+                color: Color(0xffFF1744),
+                textColor: Colors.white,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                child: const Text('Add Item to Inventory')),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Return to Main Menu'),
+            )
+          ],
+        ),
       ),
     );
   }
